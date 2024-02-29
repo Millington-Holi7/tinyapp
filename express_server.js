44ae7d85f0566d988135f3b95fb2f546e14d3997
Millington-Holi7 <holinas007@gmail.com>
2024-02-29 18:45:57 -0500

    added a app.get that pushes the urlDatabase to the template ejs file.

--- express_server.js
+++ express_server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 const urlDetabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.get("/", (req,res) => { //if this ("/") is inputted
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDetabase}
+  res.render("urls_index", templateVars)
+})
+app.get("/", (req, res) => { //if this ("/") is inputted
   res.send("Hello!"); // show this
 });
 
@@ -15,7 +20,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDetabase)
 });
 
-app.get("/Hello", (req,res) => {
+app.get("/Hello", (req, res) => {
   res.send("<html><body>Hello <b>World<b></body></html>\n")
 });
 

fd8ccc5d5b1d4a5b7f4a4d8134b1572968aa7f72
Millington-Holi7 <holinas007@gmail.com>
2024-03-05 10:32:05 -0500

    added the username to the header and add a path.

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -10,13 +12,15 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase }
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }
   res.render("urls_index", templateVars)
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -25,7 +29,7 @@ app.get("/urls/:id", (req, res) => {
   if (!longURL) { // Check if the longURL does not exist for the given id  
     return res.status(404).send("The short URL does not exist.");
   }
-  const templateVars = { id: id, longURL: longURL }
+  const templateVars = { id: id, longURL: longURL, username: req.cookies["username"] }
   res.render("urls_show", templateVars)
 });
 
@@ -47,19 +51,14 @@ app.post("/urls/:id/delete", (req, res) => {
   return res.redirect(`/urls`);
 })
 
-app.post("/urls/:id/edit", (req, res) => {
+app.post("/urls/:id", (req, res) => {
   const id = req.params.id;
   urlDatabase[id] = req.body.longURL;
   return res.redirect(`/urls`)
 })
 
-app.post("/urls/:id", (req, res) => {
-  const id = req.params.id;
-  return res.redirect(`/urls/${id}`)
-})
-
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body);
+app.post("/login", (req, res) => { //username login form 
+  res.cookie('username', req.body.username);
   return res.redirect(`/urls`);
 
 })
@@ -68,7 +67,7 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
 
-function generateRandomString() {
+function generateRandomString() { // generate random short URL
   const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let result = '';
   for (let i = 0; i < 6; i++) {

1294412a6cd6967ff36b5a6ef253a86687babfbb
Millington-Holi7 <holinas007@gmail.com>
2024-03-08 19:03:06 -0500

    added chaiHttp and tests to test the redirections and error catches.

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,33 +4,81 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
-describe('getUserByEmail', function() {
-  it('should return a user with valid email', function() {
+describe('getUserByEmail', function () {
+  it('should return a user with valid email', function () {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
     assert.equal(user.id, expectedUserID);
   });
 
-  it('should return a user object', function() {
+  it('should return a user object', function () {
     const user = getUserByEmail("user@example.com", testUsers)
-    const expectedUserID = testUsers.userRandomID;
-    assert.equal(user, expectedUserID); 
+    const expectedUser = testUsers.userRandomID;
+    assert.deepEqual(user, expectedUser);
   });
 
-  it('should return undefined if email non-existent.', function() {
+  it('should return undefined if email non-existent.', function () {
     const user = getUserByEmail("use4@example.com", testUsers)
     const expectedUserID = undefined;
-    assert.equal(user, expectedUserID); 
+    assert.equal(user, expectedUserID);
   });
 });
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../express_server');
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+
+describe('Server routes', function () {
+  const agent = chai.request.agent('http://localhost:8080');
+
+  it('GET / should redirect to /login', function () {
+    return agent
+      .get('/')
+      .then(function (res) {
+        expect(res).to.redirectTo('http://localhost:8080/login');
+        //expect(res).to.have.status(302);
+      });
+  });
+
+  it('GET /urls/new should redirect to /login', function () {
+    return agent
+      .get('/urls/new')
+      .then(function (res) {
+        expect(res).to.redirectTo('http://localhost:8080/login');
+       // expect(res).to.have.status(302);
+      });
+  });
+
+  it('GET /urls/NOTEXISTS should respond with status 404', function () {
+    return agent
+      .get('/urls/NOTEXISTS')
+      .then(function (res) {
+        expect(res).to.have.status(404);
+      });
+  });
+
+  it('GET /urls/b6UTxQ should respond with status 403', function () {
+    return agent
+      .get('/urls/b6UTxQ')
+      .then(function (res) {
+        expect(res).to.have.status(403);
+        agent.close();
+      });
+  });
+
+});
+
